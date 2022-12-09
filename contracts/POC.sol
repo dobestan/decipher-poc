b4f31b4c5cf120a1c4dc0358e45b39e6c562de3b
Suchan An <dobestan@gmail.com>
2022-12-10 00:01:46 +0900

    Implement POC Contract with sample poc-specific method

--- contracts/POC.sol
+++ contracts/POC.sol
@@ -8,4 +8,9 @@ import "./extensions/ERC20Epochs.sol";
 contract POC is ERC20Epochs {
     constructor() ERC20Epochs("POC", "POC") {
     }
+
+    // Examples for POC(Proof of Contribution) specific logics.
+    function decimals() public pure override returns (uint8) {
+        return 0;
+    }
 }
