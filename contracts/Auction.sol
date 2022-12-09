d5f4d418d2fd1b0ea948f04801d07619bdc3c1d1
Suchan An <dobestan@gmail.com>
2022-12-10 01:25:24 +0900

    Implement Auction Contract with Auction CRUD features

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -8,6 +8,11 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract POCAuction {
     using Counters for Counters.Counter;
 
+    event AuctionCreated(
+        uint indexed id,
+        address indexed maker
+    );
+
     enum Status {
         Created,
         Ended
@@ -27,4 +32,34 @@ contract POCAuction {
     
     Counters.Counter private _id;
     Auction[] private _auctions;
+
+    function totalAuctionsCount() public view returns (uint) {
+        return _id.current();
+    }
+
+    function getAuction() public view returns (Auction memory) {
+        return _auctions[_id.current() - 1];
+    }
+
+    function getAuction(uint id) public view returns (Auction memory) {
+        return _auctions[id];
+    }
+
+    function createAuction() public returns (uint ) {
+        uint currentId = _id.current();
+
+        Auction memory newAuction = Auction(
+            currentId,
+            Status.Created,
+            msg.sender,
+            address(0),
+            0
+        );
+        _auctions.push(newAuction);
+
+        emit AuctionCreated(currentId, msg.sender);
+
+        _id.increment();
+        return currentId;
+    }
 }
